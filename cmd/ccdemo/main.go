e5a985b73d92e7de247ccccf62869c642f5126bb
Tomas Holm <tomas.holm@sinch.com>
2022-09-02 08:54:37 +0200

    Added health response

--- cmd/ccdemo/main.go
+++ cmd/ccdemo/main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Create a simple HTTP server for Kubernetes health checks
+	router := gin.Default()
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"Status": "OK",
+		})
+	})
+	router.Run()
+
 	for {
 		fmt.Println("Hello World!")
 		time.Sleep(5 * time.Second)
